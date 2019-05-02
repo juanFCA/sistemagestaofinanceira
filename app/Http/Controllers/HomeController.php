f33f5501c0f7b6de07d0223e7807c159f69aa26a
Juan Ferreira Carlos <juanfcarlos.93@gmail.com>
2019-05-01 21:03:39 -0300

    Tela de Dashboard Integrada Mostrando Total Receita e Despesa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Despesa;
+use App\Receita;
+use Illuminate\Support\Facades\Auth;
+use App\Charts\ReceitaCategoria;
+use App\Charts\DespesaCategoria;
+use App\Categoria;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,56 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $totalReceitas = $this->totalReceitas();
+        $totalDespesas = $this->totalDespesas();
+
+        //$receitaCategoriaChart = $this->receitaCategoriaChart();
+        //$despesaCategoriaChart = $this->despesaCategoriaChart();
+
+        return view('home', compact('totalReceitas','totalDespesas'));
+    }
+
+    private function totalReceitas() {
+        $receitasColecao = Receita::where('user_id', Auth::user()->id)->get();
+        $soma = 0;
+        foreach ($receitasColecao as $key => $value) {
+            $soma += $value->valor;
+        }
+        return $soma;
+    }
+
+    private function totalDespesas() {
+        $despesasColecao = Despesa::where('user_id', Auth::user()->id)->get();
+        $soma = 0;
+        foreach ($despesasColecao as $key => $value) {
+            $soma += $value->valor;
+        }
+        return $soma;
+    }
+
+    private function receitaCategoriaChart() {
+        $categoriasReceitaNome = Categoria::where('user_id', Auth::user()->id)->where('receita', 1)->pluck('nome');
+        $categoriasReceitaValor = Receita::where('user_id', Auth::user()->id)->pluck('valor');
+
+        $chart = new ReceitaCategoria;
+        $chart->labels($categoriasReceitaNome);
+        $chart->dataset('Valores', 'doughnut', $categoriasReceitaValor);
+        $chart->displayAxes(false, false);
+        $chart->minimalist(true);
+
+        return $chart;
+    }
+
+    private function despesaCategoriaChart() {
+        $categoriasDespesaNome = Categoria::where('user_id', Auth::user()->id)->where('receita', 0)->pluck('nome');
+        $categoriasDespesaValor = Despesa::where('user_id', Auth::user()->id)->pluck('valor');
+
+        $chart = new DespesaCategoria;
+        $chart->labels($categoriasDespesaNome);
+        $chart->dataset('Valores', 'doughnut', $categoriasDespesaValor);
+        $chart->displayAxes(false, false);
+        $chart->minimalist(true);
+
+        return $chart;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
 
+    <!-- Se existirem erros a serem mostrados exibe aqui -->
+    @if ($errors->any())
+        <div class="alert alert-danger" id="divalert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <!-- Se existirem sucessos a serem mostrados exibe aqui -->
+    @if(session()->get('success'))
+        <div class="alert alert-success" id="divalert">
+            {{ session()->get('success') }}  
+        </div>
+    @endif
+
+    <div class="row">
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Receitas a receber</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">R$ {{ $totalReceitas }}</div>
+                </div>
+                <div class="col-auto">
+                <i class="fas fa-arrow-alt-circle-up text-info fa-2x"></i>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-danger shadow h-100 py-2">
+            <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Despesas a pagar</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">R$ {{ $totalDespesas }}</div>
+                </div>
+                <div class="col-auto">
+                <i class="fas fa-arrow-alt-circle-down text-danger fa-2x"></i>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card shadow">
+                <div class="card-header bg-info text-xs text-uppercase text-white font-weight-bold">
+                    Receita / Categoria
+                </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <canvas id="receita-categoria" height="250" width="250"></canvas>
                 </div>
             </div>
         </div>
+
+        <div class="col-md-6">
+        <div class="card shadow">
+            <div class="card-header bg-danger text-xs text-uppercase text-white font-weight-bold">
+                    Despesa / Categoria
+            </div>
+            <div class="card-body">
+                    <canvas id="despesa-categoria" height="250" width="250"></canvas>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
